b9ed360591143eee6d1a6e4b0df840de34a1abad
realtouseef <touseef69r@gmail.com>
2024-12-31 13:01:42 +0500

    update: routes added

--- configs/routes.ts
+++ configs/routes.ts
@@ -1,8 +1,6 @@
 export const ROUTES = {
-  auth: process.env.NEXT_PUBLIC_URL
-    ? `${process.env.NEXT_PUBLIC_URL}/authentication`
-    : "/authentication",
-  dashboard: process.env.NEXT_PUBLIC_URL
-    ? `${process.env.NEXT_PUBLIC_URL}/dashboard/hotels`
-    : "/dashboard/hotels",
+  // ----------------- Auth Routes -----------------
+  auth: `/authentication`,
+  // ----------------- App Routes -----------------
+  dashboard: `/dashboard/hotels`,
 };
