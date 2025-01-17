5cc9864e6647e901729e9c3bceefb7a2230050d6
realtouseef <touseef69r@gmail.com>
2025-01-17 11:13:07 +0500

    update: success page done

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,14 +1,44 @@
-import { authOptions } from "@Jetzy/app/api/auth/[...nextauth]/authOptions"
-import { ROUTES } from "@Jetzy/configs/routes"
-import { getServerSession } from "next-auth"
-import { redirect } from "next/navigation"
+import theme from "@/theme"
+import ReduxProvider from "@Jetzy/redux/ReduxProvider"
+import type { Metadata } from "next"
+import { NuqsAdapter } from "nuqs/adapters/next"
+import { ChakraProvider } from "@chakra-ui/react"
+import { ThemeProvider } from "../providers/theme-provider"
+import AppSessionProvider from "../providers/SessionProvider"
+import "@/app/globals.css"
 
-export default async function AuthLayout({ children }: { children: React.ReactNode }) {
-	const session = await getServerSession(authOptions)
 
-	if (session) {
-		return redirect(ROUTES.dashboard)
-	}
+export const metadata: Metadata = {
+	title: "Jetzy",
+	description: "The world's first geo-based social app to connect you with travel and lifestyle enthusiasts with similar interests.",
+	other: {
+		"http-equiv": "Permissions-Policy",
+		content: "interest-cohort=()",
+	},
+}
 
-	return <section>{children}</section>
+export default async function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html lang="en">
+			<ThemeProvider theme={theme}>
+				<body className={`antialiased`}>
+					<ChakraProvider>
+						<NuqsAdapter>
+							<div className="flex flex-col min-h-screen">
+								<main className="flex-1">
+									<AppSessionProvider>
+										<ReduxProvider>{children}</ReduxProvider>
+									</AppSessionProvider>
+								</main>
+							</div>
+						</NuqsAdapter>
+					</ChakraProvider>
+				</body>
+			</ThemeProvider>
+		</html>
+	)
 }
---
+++ app/(pages)/success/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+
+import { Typography } from 'antd'
+
+import AppleJetzyApp from '@/app/assets/images/auth/explanation-section/apple-jetzy-app.png'
+import GoogleJetzyApp from '@/app/assets/images/auth/explanation-section/google-jetzy-app.png'
+
+import JetzyBadge from '@/app/assets/images/auth/success/badge.png'
+
+const Success = () => {
+  return (
+    <>
+    <div className='flex flex-col items-center justify-center gap-y-8'>
+      <Image src={JetzyBadge} alt='jetzy pro membership badge' className='w-[267px] h-[267px]' />
+      <div className='flex flex-col w-[420px] items-center justify-center text-center'>
+      <Typography.Text className='text-[32px] font-bold text-[#222B38]'>Congratulations!</Typography.Text>
+      <Typography.Text className='mt-2 mb-5 text-[18px] font-bold text-[#222B38]'>You've Been Awarded a Jetzy Pro Subscription.</Typography.Text>
+      <Typography.Text className='text-base font-normal text-[#707C8D]'>Welcome to the premium Jetzy experience, exclusively tailored for you. You’ve been gifted a subscription worth $135 – enjoy unparalleled perks and benefits!</Typography.Text>
+      </div>
+      <div className='border rounded-xl py-2 px-3 w-[388px] space-y-1'>
+        <div className='flex items-center justify-between'>
+        <Typography.Text className='text-[20px] font-bold text-black'>Alex Brown</Typography.Text>
+        <Typography.Text className='text-base text-[#07B75F] italic'>Active</Typography.Text>
+        </div>
+        <div className='flex items-center justify-between'>
+        <Typography.Text className='text-base font-normal text-black'>Full Concierge Plan</Typography.Text>
+        <Typography.Text className='text-base font-normal text-black'>$135</Typography.Text>
+        </div>
+        <div className='flex items-center justify-between'>
+        <Typography.Text className='text-[15px] text-[#707C8D] font-normal'>Start Date: 01/01/2024</Typography.Text>
+        <Typography.Text className='text-[15px] text-[#707C8D] font-normal'>End Date: 01/01/2024</Typography.Text>
+        </div>
+      </div>
+      <Typography.Text className='text-[#222B38] text-[18px] font-bold'>Login to the app to unlock your VIP perks!</Typography.Text>
+
+      <div className='flex gap-x-2 mb-10 w-[300px]'>
+        <a href='/'><Image src={AppleJetzyApp} alt='jetzy mobile app ios'  /></a>
+        <a href='/'><Image src={GoogleJetzyApp} alt='jetzy mobile app android'  /></a>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Success
